c5d6b5b6e0ed970ab85d987f3215541d2dfaf006
Scott McKay <evilscaught+gitlab@gmail.com>
2020-07-11 15:49:14 -0600

    Commented out baseUrl configurations in vue.config.js

--- evilscaught-web/vue.config.js
+++ evilscaught-web/vue.config.js
@@ -1,7 +1,12 @@
 // vue.config.js file to be place in the root of your repository
 
+// Defines the starting point of the base URL on GitLab pages.
+// i.e. https://evilscaught.gitlab.io/CI_PROJECT_NAME/
+
+/*
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production'
     ? '/' + process.env.CI_PROJECT_NAME + '/'
     : '/'
 }
+*/
